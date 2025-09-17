d374ada110cc97506edc0400db3bbacfda007569
Miguel <migueldelalibera9@gmail.com>
2025-09-17 15:34:06 -0300

    First commit

---
+++ src/main/java/com/example/gerenciamento_tickets/GerenciamentoTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_tickets/GerenciamentoTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
