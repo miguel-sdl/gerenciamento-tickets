8729da27e2c2773c1373eb3b8f75d49de71070db
Miguel <migueldelalibera9@gmail.com>
2025-09-25 23:54:09 -0300

    fix: faz correcao de nome de prazoDefaultEmHoras em CategoriaResponseBody

--- src/main/java/com/example/gerenciamento_tickets/dto/CategoriaResponseBody.java
+++ src/main/java/com/example/gerenciamento_tickets/dto/CategoriaResponseBody.java
@@ -2,6 +2,6 @@ package com.example.gerenciamento_tickets.dto;
 
 import java.util.List;
 
-public record CategoriaResponseBody(long id, String nome, int prazoDefaulEmHoras,
+public record CategoriaResponseBody(long id, String nome, int prazoDefaultEmHoras,
                                     List<UsuarioResponseBody> usuariosResponsaveis) {
 }
